69fb80278390e55acd344df0705c932df3d0ce5f
anhphong22 <74782468+anhphong22@users.noreply.github.com>
2023-05-04 11:41:16 +0700

    feat: update port webapp (public)

--- logdev/config.py
+++ logdev/config.py
@@ -1,57 +1,63 @@
-import logging
-import os
-import sys
 from collections import defaultdict
 from contextlib import contextmanager
-
+import os
+import logging
+import sys
 import commentjson as json
-
-from . import shared
 
 __all__ = [
     "my_api_key",
-    "auth_flag",
+    "authflag",
     "auth_list",
     "dockerflag",
     "retrieve_proxy",
     "log_level",
     "advance_docs",
     "update_doc_config",
+    "render_latex",
+    "usage_limit",
     "multi_api_key",
+    "server_name",
+    "server_port",
+    "share",
+    "hide_history_when_not_logged_in"
 ]
 
-# Add a unified config file to avoid confusion caused by too many files (the lowest priority)
-# At the same time, it can also provide config help for subsequent support for custom functions
+from logdev import shared, constants
+
 if os.path.exists("config.json"):
-    with open("config.json", "r", encoding="utf-8") as f:
+    with open("config.json", "r", encoding='utf-8') as f:
         config = json.load(f)
 else:
     config = {}
+
+lang_config = config.get("language", "auto")
+language = os.environ.get("LANGUAGE", lang_config)
+
+hide_history_when_not_logged_in = config.get("hide_history_when_not_logged_in", False)
 
 if os.path.exists("api_key.txt"):
     logging.info("api_key.txt file detected, migration in progress...")
     with open("api_key.txt", "r") as f:
         config["openai_api_key"] = f.read().strip()
     os.rename("api_key.txt", "api_key(deprecated).txt")
-    with open("config.json", "w", encoding="utf-8") as f:
+    with open("config.json", "w", encoding='utf-8') as f:
         json.dump(config, f, indent=4)
 
 if os.path.exists("auth.json"):
     logging.info("auth.json file detected, migration in progress...")
     auth_list = []
-    with open("auth.json", "r", encoding="utf-8") as f:
+    with open("auth.json", "r", encoding='utf-8') as f:
         auth = json.load(f)
         for _ in auth:
             if auth[_]["username"] and auth[_]["password"]:
                 auth_list.append((auth[_]["username"], auth[_]["password"]))
             else:
-                logging.error(
-                    "Please check the username and password in the `auth.json` file!"
-                )
+                logging.error("Please check the username and password in the auth.json file!")
                 sys.exit(1)
     config["users"] = auth_list
     os.rename("auth.json", "auth(deprecated).json")
-    with open("config.json", "w", encoding="utf-8") as f:
+    with open("config.json", "w", encoding='utf-8') as f:
         json.dump(config, f, indent=4)
 
 # Dealing with docker if we are running in Docker
@@ -59,43 +65,34 @@ dockerflag = config.get("dockerflag", False)
 if os.environ.get("dockerrun") == "yes":
     dockerflag = True
 
-# Handle api-key and allowed user list
 my_api_key = config.get("openai_api_key", "")
-my_api_key = os.environ.get("my_api_key", my_api_key)
+my_api_key = os.environ.get("OPENAI_API_KEY", my_api_key)
 
-# Multi-account mechanism
-multi_api_key = config.get(
-    "multi_api_key", False
-)  # Whether to enable the multi-account mechanism
+xmchat_api_key = config.get("xmchat_api_key", "")
+os.environ["XMCHAT_API_KEY"] = xmchat_api_key
+
+render_latex = config.get("render_latex", False)
+
+if render_latex:
+    os.environ["RENDER_LATEX"] = "yes"
+else:
+    os.environ["RENDER_LATEX"] = "no"
+
+usage_limit = os.environ.get("USAGE_LIMIT", config.get("usage_limit", 120))
+
+multi_api_key = config.get("multi_api_key", False)
 if multi_api_key:
     api_key_list = config.get("api_key_list", [])
     if len(api_key_list) == 0:
-        logging.error(
-            "The multi-account mode is enabled, but the `api_key_list` is empty, please check `config.json`"
-        )
+        logging.error("The multi-account mode is enabled, but the api_key_list is empty, please check config.json")
         sys.exit(1)
     shared.state.set_api_key_queue(api_key_list)
 
-auth_list = config.get("users", [])  # is actually a list of users
-# Whether to enable the status value of authentication, change to judge the length of auth_list
-auth_flag = len(auth_list) > 0
-
-# Process the custom api_host, read the configuration of the environment variable first,
-# and automatically assemble it if it exists
+auth_list = config.get("users", [])
+authflag = len(auth_list) > 0
 api_host = os.environ.get("api_host", config.get("api_host", ""))
 if api_host:
     shared.state.set_api_host(api_host)
-
-if dockerflag:
-    if my_api_key == "empty":
-        logging.error("Please give a api key!")
-        sys.exit(1)
-    # auth
-    username = os.environ.get("USERNAME")
-    password = os.environ.get("PASSWORD")
-    if not (isinstance(username, type(None)) or isinstance(password, type(None))):
-        auth_list.append((os.environ.get("USERNAME"), os.environ.get("PASSWORD")))
-        auth_flag = True
 
 
 @contextmanager
@@ -110,32 +107,28 @@ def retrieve_openai_api(api_key=None):
     os.environ["OPENAI_API_KEY"] = old_api_key
 
 
-# Handle proxy:
-http_proxy = config.get("http_proxy", "")
-https_proxy = config.get("https_proxy", "")
-http_proxy = os.environ.get("HTTP_PROXY", http_proxy)
-https_proxy = os.environ.get("HTTPS_PROXY", https_proxy)
-
-# Reset the system variable, do not set the environment variable when it does not need to be set,
-# so as not to cause the global agent to report an error
-os.environ["HTTP_PROXY"] = ""
-os.environ["HTTPS_PROXY"] = ""
-
-# Whether to use local embedding
-local_embedding = config.get("local_embedding", False)
 log_level = config.get("log_level", "INFO")
 logging.basicConfig(
     level=log_level,
     format="%(asctime)s [%(levelname)s] [%(filename)s:%(lineno)d] %(message)s",
 )
 
+http_proxy = config.get("http_proxy", "")
+https_proxy = config.get("https_proxy", "")
+http_proxy = os.environ.get("HTTP_PROXY", http_proxy)
+https_proxy = os.environ.get("HTTPS_PROXY", https_proxy)
+
+os.environ["HTTP_PROXY"] = ""
+os.environ["HTTPS_PROXY"] = ""
+
+local_embedding = config.get("local_embedding", False)
+
 
 @contextmanager
 def retrieve_proxy(proxy=None):
     """
-    Logic of proxy
-    1. If proxy = NONE, set the environment variable and return the latest set proxy
-    2. If proxy! = NONE, update the current proxy configuration, but do not update environment variables
+     1. If proxy = NONE, set the environment variable and return the latest set proxy
+     2. If proxy! = NONE, update the current proxy configuration, but do not update environment variables
     """
     global http_proxy, https_proxy
     if proxy is not None:
@@ -152,7 +145,6 @@ def retrieve_proxy(proxy=None):
         os.environ["HTTP_PROXY"], os.environ["HTTPS_PROXY"] = old_var
 
 
-# Handling advance docs
 advance_docs = defaultdict(lambda: defaultdict(dict))
 advance_docs.update(config.get("advance_docs", {}))
 
@@ -161,4 +153,27 @@ def update_doc_config(two_column_pdf):
     global advance_docs
     advance_docs["pdf"]["two_column"] = two_column_pdf
 
-    logging.info(f"The updated file parameters are：{advance_docs}")
+    logging.info(f"The updated documentation parameter is: {advance_docs}")
+
+
+server_name = config.get("server_name", None)
+server_port = config.get("server_port", None)
+if server_name is None:
+    if dockerflag:
+        server_name = "0.0.0.0"
+    else:
+        server_name = "127.0.0.1"
+if server_port is None:
+    if dockerflag:
+        server_port = 7860
+
+assert server_port is None or type(server_port) == int, "requires `port` to be set to int type"
+
+
+default_model = config.get("default_model", "")
+try:
+    constants.DEFAULT_MODEL = constants.MODELS.index(default_model)
+except ValueError:
+    pass
+
+share = config.get("share", False)
